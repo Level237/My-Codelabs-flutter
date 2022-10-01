04a23f56950b732f964c8ada08084f4ace1e7d5a
level237 <Bramslevel129@gmail.com>
2022-10-01 10:38:09 -0400

    list like name generator

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,9 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     
     final wordPair=WordPair.random();
-    return MaterialApp(
+    return const MaterialApp(
       title: "Startup Name Generator",
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Startup Name Generator") ,
-        ),
-        body: const Center(
-          child: RandomWords(),
-          ),
-        ),
+      home: RandomWords(),
         
     );
   }
@@ -40,12 +33,58 @@ class _RandomWordsState extends State<RandomWords> {
   final _sugestions=<WordPair>[];
   final _biggerFont=const TextStyle(fontSize: 18);
   final _saved=<WordPair>{};
+
+    void _pushSaved() {
+    Navigator.of(context).push(
+      // Add lines from here...
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+                  context: context,
+                  tiles: tiles,
+                ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ), // ...to here.
+    );
+  }
   @override
   Widget build(BuildContext context) {
 
     final wordpair=WordPair.random();
 
-    return ListView.builder(
+
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Startup Name Generator"),
+        actions: [
+          IconButton(
+            onPressed: _pushSaved, 
+            icon: const Icon(Icons.list),
+            tooltip: 'Saved sugestions',
+            )
+        ],
+      ),
+      body:ListView.builder(
       padding: const EdgeInsets.all(16.0),
       itemBuilder: (context, i) {
         if (i.isOdd) return const Divider();
@@ -56,17 +95,36 @@ class _RandomWordsState extends State<RandomWords> {
         }
         final alreadySaved=_saved.contains(_sugestions[index]);
         return ListTile(
+          leading:const Icon(Icons.wallet_membership_outlined),
+          
           title: Text(
             _sugestions[index].asPascalCase,
             style: _biggerFont,
           ),
           trailing: Icon(
             alreadySaved? Icons.favorite : Icons.favorite_border,
+            
             color: alreadySaved? Colors.red : null,
             semanticLabel: alreadySaved? 'Remove from saved' : "save",
           ),
+          onTap: (){
+            setState(() {
+              if(alreadySaved){
+                _saved.remove(_sugestions[index]);
+              }else{
+                _saved.add(_sugestions[index]);
+                print(alreadySaved);
+              }
+            });
+          },
         );
       },
+      )
     );
+    
+    
+    
+    
+    
   }
 }
