1225f7a3f7340001f563af1c9c73b8a3865ba8b2
level237 <Bramslevel129@gmail.com>
2022-10-01 15:21:48 -0400

    update

--- webview_in_flutter/lib/main.dart
+++ webview_in_flutter/lib/main.dart
@@ -1,27 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'src/web_view_stack.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+  runApp(
+    const MaterialApp(
       home: WebViewApp(),
-    );
-  }
+    ),
+  );
 }
+
 class WebViewApp extends StatefulWidget {
-  const WebViewApp({super.key});
+  const WebViewApp({Key? key}) : super(key: key);
 
   @override
   State<WebViewApp> createState() => _WebViewAppState();
@@ -31,12 +20,10 @@ class _WebViewAppState extends State<WebViewApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title: const Text('Flutter Webview'),
+      appBar: AppBar(
+        title: const Text('Flutter WebView'),
       ),
-      body: const WebView(
-        initialUrl: 'https://flutter.dev',
-      ),
+      body: const WebViewStack(),   // Replace the WebView widget with WebViewStack
     );
   }
 }
